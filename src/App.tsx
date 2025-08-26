c8079a42bfe16832eac5d99fe2e1476b612e94a6
juani <Jhigonet89@gmail.com>
2025-08-26 15:34:56 -0300

    Diseno metodos de pago de entradas

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import Login from './components/Login';
 import Register from './components/Register';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
-import './App.css';
-import Footer from './components/Footer';
 import EventoDetalle from './components/EventoDetalle';
+import PaginaPago from './pages/PaginaPago'; // Asegúrate que la ruta sea correcta
 import ScrollToTop from './components/ScrollToTop';
+import Footer from './components/Footer';
+import './App.css';
+import CompraExitosa from './pages/CompraExitosa';
 
+
+// Componentes temporales
 const MisEntradas = () => <h2>Mis Entradas (Página en construcción)</h2>;
 const Configuracion = () => <h2>Configuración (Página en construcción)</h2>;
 
@@ -19,15 +23,18 @@ function App() {
             <ScrollToTop />
             <Navbar />
             <main className="content-wrap">
+                {/* El componente <Routes> gestiona qué página mostrar */}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/evento/:id" element={<EventoDetalle />} />
+                    <Route path="/evento/:id/comprar" element={<PaginaPago />} /> {/* <-- RUTA AÑADIDA */}
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/forgot-password" element={<ForgotPassword />} />
                     <Route path="/reset-password/:token" element={<ResetPassword />} />
                     <Route path="/mis-entradas" element={<MisEntradas />} />
                     <Route path="/configuracion" element={<Configuracion />} />
+                    <Route path="/compra-exitosa" element={<CompraExitosa />} />
                 </Routes>
             </main>
             <Footer /> 
